3a20b33af128250f301f292d44104c046ef037f1
Alexisp1217i <aldabaalexis85@gmail.com>
2024-11-08 17:01:35 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg24550824_examen_evaluacion2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int piedra=1,papel=2,tijeras=3;
+       Scanner scani=new Scanner(System.in);
+        System.out.println("introducir Numero del 1-3: ");
+        scani.nextLine();
+        System.out.println(Math.random());
+       
+        for (int i = piedra; i < papel; i++) {
+            if () {
+                
+            }
+        }
+ 
+    }
+    
+}
